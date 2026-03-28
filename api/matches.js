e3a57f58e5c37d4c5c219977d596f9401a37ef84
Gfuryh7 <iamtitanic0@gmail.com>
2026-03-28 17:58:40 +0000

    Added backend API route

---
+++ api/matches.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+
+  const response = await fetch("https://v3.football.api-sports.io/fixtures?league=39&season=2024", {
+    headers: {
+      "x-apisports-key": process.env.API_KEY
+    }
+  });
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
